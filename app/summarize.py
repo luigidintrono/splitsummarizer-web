38cec7a63fabf7f2e75ce14bea2e018354348bf4
Abhik Pal <abhikpal2509@gmail.com>
2016-07-13 18:03:35 +0530

    added lexrank from sumy

--- app/summarize.py
+++ app/summarize.py
@@ -1,9 +1,24 @@
 """
 Main script that does summarizing for us.
 """
+from sumy.parsers.plaintext import PlaintextParser
+from sumy.nlp.tokenizers import Tokenizer 
+from sumy.summarizers.lex_rank import LexRankSummarizer 
+
+
 
 def get_summary(text):
     """
     Given some input text, returns its summary.
     """
-    return text.upper()
+    summarized = ''
+    parser = PlaintextParser.from_string(text, Tokenizer("english"))
+    summarizer = LexRankSummarizer()
+
+    num_lines = text.count('.')
+    summary = summarizer(parser.document, round(num_lines/3))
+
+    for sentence in summary:
+        summarized += str(sentence) + ' '
+
+    return summarized   
