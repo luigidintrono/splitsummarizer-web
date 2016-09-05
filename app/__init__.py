8ffea6573ace225a8b293dd6c7f8c98c1393dd00
Abhik Pal <theabhikpal@gmail.com>
2016-09-05 16:53:16 +0530

    Basic bootstrap up.

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,21 @@
 from flask import Flask
+from flask_bootstrap import Bootstrap
+from flask_nav import Nav
+from flask_nav.elements import Navbar, View
+
+nav = Nav()
+
+@nav.navigation()
+def frontend_top():
+    return Navbar(
+        'Split Summarizer',
+        View('Home', 'index'),
+    )
+
 
 app = Flask(__name__)
 app.config.from_object('config')
+Bootstrap(app)
 
 from app import views
+nav.init_app(app)
