99c9baffc4bc9f40492af1ecfb649aefaaef1b7a
Abhik Pal <theabhikpal@gmail.com>
2016-09-10 17:35:21 +0530

    Remove bootstrap and nav imports from main flask app.

--- app/__init__.py
+++ app/__init__.py
@@ -1,23 +1,6 @@
 from flask import Flask
-from flask_bootstrap import Bootstrap
-from flask_nav import Nav
-from flask_nav.elements import Navbar, Link
-
-nav = Nav()
-
-@nav.navigation()
-def frontend_top():
-    return Navbar(
-        'Split Summarizer',
-        Link('Home', '/index/'),
-        Link('About', '/about/'),
-        Link('Contact', '/contact/')
-        )
-
 
 app = Flask(__name__)
 app.config.from_object('config')
-Bootstrap(app)
 
 from app import views
-nav.init_app(app)
