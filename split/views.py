f838a0b638e269cf34fa0e8042ad81f334d8a278
Abhik Pal <theabhikpal@gmail.com>
2016-09-16 19:26:09 +0530

    Restructured summary view and input

--- split/views.py
+++ split/views.py
@@ -6,8 +6,19 @@ import summarize
 
 @application.route('/', methods=['GET', 'POST'])
 def index():
+    return render_template("index.html", title="Home")
+
+@application.route('/summarize/', methods=['GET', 'POST'])
+def summarizer():
     if request.method == 'POST':
         source_text = request.form['source_text']
         summary = summarize.get_summary(source_text)
-        return render_template("index.html", title="Summary", summary=summary)
-    return render_template("index.html", title="Home")
+        return render_template("summary.html",
+                                title="Summary",
+                                summary=summary)
+    return render_template("summary_form.html", title="Summarize")
+
+@application.route('/about/')
+def about():
+    return render_template("index.html", title="About")
+
