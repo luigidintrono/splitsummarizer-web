01598ee3dd0d3be48dc4402daa4b29874c6c8efb
luigidintrono <luisdaviddg@hotmail.com>
2016-12-13 19:11:37 -0700

    split-new-frontend

---
+++ app.js
@@ -0,0 +1,14 @@
+function animation(){
+   $("#logo").hide().show("slow");
+   $(".nav li").hide().show("slow");
+}
+$(document).ready(animation);
+
+$("#rating").click(function(){
+		$("#rating").hide("slow");
+	})
+
+
+
+
+
